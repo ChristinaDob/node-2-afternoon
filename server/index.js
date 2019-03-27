1433fee5143ea8975963fbd3325e3948ece9239e
Christina Dob <cd@cd.local>
2019-03-26 19:36:36 -0700

    Week 4 Day 2 Project

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-// we can import our controller into index.js and create endpoints that will execute the logic.
+// we can import our controller into index.js and create endpoints
+// that will execute the logic.
 // In this step, we will hook up our controller to our app in server/index.js
 // Require the messages controller.
 const mc = require('./controllers/messages_controller');
@@ -14,10 +15,13 @@ app.use(express.json());
 app.use(express.static(__dirname + '/../public/build'));
 
 // The url for this api should be /api/messages.
-// make a messagesBaseUrl variable so that if the URL ever changes we won't have to update in four different places. The messagesBaseUrl should equal /api/messages.
+// make a messagesBaseUrl variable so that if the URL ever changes we won't have
+// to update in four different places. The messagesBaseUrl should equal /api/messages.
 const messagesBaseUrl = '/api/messages';
-// Create a post, get, put, and delete endpoint that use the corresponding method on the messages controller.
-// For the put and delete endpoints, we need to add on a url parameter of id. A url paramter can be defined by adding :variableName when making the URL for an endpoint.
+// Create a post, get, put, and delete endpoint that use the corresponding method on
+// the messages controller.
+// For the put and delete endpoints, we need to add on a url parameter of id.
+// A url paramter can be defined by adding :variableName when making the URL for an endpoint.
 app.post(messagesBaseUrl, mc.create);
 app.get(messagesBaseUrl, mc.read);
 app.put(`${messagesBaseUrl}/:id`, mc.update);
